0e4182817b04695455101ceb43152671747f0d22
najrin-git <renish@mail.bruva.co>
2024-06-12 18:42:50 +0000

    Update and rename biisal/server/__init__.py to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
-# © agrprojects
-
 from aiohttp import web
-from .stream_routes import routes
 
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Mᴋɴ Bᴏᴛᴢ")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
